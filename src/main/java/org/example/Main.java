5054a755c1c8bd4d2fefd234fed1a5442dc4f861
Praveen052023 <praveenreddypndyla@gmail.com>
2023-05-25 09:49:06 +0530

    test1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,10 +1,7 @@
 package org.example;
 
-import java.io.IOException;
-import java.net.URISyntaxException;
-
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello Praveen");
+        System.out.println("hello Praveen !!!");
     }
 }
